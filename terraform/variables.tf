54238b0dc3214dffe3423f01962a47dc1cd7eef6
TK <tyagraj@gmail.com>
2025-02-06 10:55:49 +0530

    Create variables.tf
    variables file

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "vm_memory" {
+  description = "Memory allocated to the VM"
+  default     = 2048
+}
+
+variable "vm_cpus" {
+  description = "Number of CPUs allocated to the VM"
+  default     = 2
+}
